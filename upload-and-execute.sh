c81b7600e7305024c6f7a70c702a7b8ef3811b13
Ta Quy <taquy.se@gmail.com>
2024-04-22 01:06:16 -0400

    add apache server and its exporter

--- upload-and-execute.sh
+++ upload-and-execute.sh
@@ -2,4 +2,6 @@
 
 rsync -avz -e "ssh -i \"$HOME/.ssh/maas\""  --exclude='.git' ./ qt@192.168.64.2:~/qt-docker-composes 
 
+ssh -i $HOME/.ssh/maas qt@192.168.64.2 "docker-compose -f ~/qt-docker-composes/loki-prometheus-grafana.yaml down --remove-orphans"
+
 ssh -i $HOME/.ssh/maas qt@192.168.64.2 "docker-compose -f ~/qt-docker-composes/loki-prometheus-grafana.yaml up"
