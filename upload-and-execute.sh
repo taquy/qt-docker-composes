31ca93593b69d8fe702e051e11c976331102421c
Ta Quy <taquy.se@gmail.com>
2024-06-03 14:53:22 -0400

    add server status endpoint

--- upload-and-execute.sh
+++ upload-and-execute.sh
@@ -2,8 +2,10 @@
 
 export HOSTNAME='192.168.66.5'
 
+export TARGET_DIR=/home/qt/qt-docker-composes
+
 rsync -avz -e "ssh -i \"$HOME/.ssh/maas\""  --exclude='.git' ./ qt@$HOSTNAME:~/qt-docker-composes 
 
-ssh -i $HOME/.ssh/maas qt@$HOSTNAME "docker-compose down --remove-orphans"
+ssh -i $HOME/.ssh/maas qt@$HOSTNAME "cd $TARGET_DIR && docker-compose down --remove-orphans"
 
-ssh -i $HOME/.ssh/maas qt@$HOSTNAME "docker-compose up"
+ssh -i $HOME/.ssh/maas qt@$HOSTNAME "cd $TARGET_DIR && docker-compose up"
